76d14624257a190234c0a8c02ea46b0b8622aae3
Ayas Nasih <ayasnasih@gmail.com>
2021-08-06 20:34:21 +0500

    Create MembershipCard.java

---
+++ src/villalibraryms/Models/MembershipCard.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package villalibraryms.Models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ayasnasih
+ */
+public class MembershipCard {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public Date getIssuedAt() {
+        return issuedAt;
+    }
+
+    public void setIssuedAt(Date issuedAt) {
+        this.issuedAt = issuedAt;
+    }
+
+    public Date getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(Date expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    private int id;
+    private String barcode;
+    private Date issuedAt;
+    private Date expiresAt;
+    private int userId;
+
+    public MembershipCard(int id, String barcode, Date issuedAt, Date expiresAt, int userId) {
+        this.id = id;
+        this.barcode = barcode;
+        this.issuedAt = issuedAt;
+        this.expiresAt = expiresAt;
+        this.userId = userId;
+    }
+    
+}
