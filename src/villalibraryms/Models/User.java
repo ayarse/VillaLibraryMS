621853d0e335dfd98a371abc057eea0b2e1d7700
Ayas Nasih <ayasnasih@gmail.com>
2021-08-06 20:34:13 +0500

    Create User.java

---
+++ src/villalibraryms/Models/User.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package villalibraryms.Models;
+
+/**
+ *
+ * @author ayasnasih
+ */
+public class User {
+
+    int id;
+    String username;
+    String password;
+    int roleId;
+    String displayName;
+    boolean isActive;
+    MembershipCard membershipCard;
+
+    public User(int id, String username, String password, int roleId, String displayName, boolean isActive) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.roleId = roleId;
+        this.displayName = displayName;
+        this.isActive = isActive;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public Role getRole() {
+        return Role.find(this.roleId);
+    }
+
+    public void setRole(Role role) {
+        this.roleId = role.id;
+    }
+
+    public void setMembershipCard(MembershipCard card) {
+        this.membershipCard = card;
+    }
+
+    public MembershipCard getMembershipCard() {
+        return this.membershipCard;
+    }
+}
