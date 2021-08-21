7218481c4caafc98e314f62a73314fcd83d04377
Ayas Nasih <ayasnasih@gmail.com>
2021-08-21 06:29:19 +0500

    Update library_db.sql

--- library_db.sql
+++ library_db.sql
@@ -27,7 +27,7 @@ CREATE TABLE `authors` (
   `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,7 @@ CREATE TABLE `authors` (
 
 LOCK TABLES `authors` WRITE;
 /*!40000 ALTER TABLE `authors` DISABLE KEYS */;
-INSERT INTO `authors` VALUES (31,'Sherlene Pfannerstill'),(32,'Ulysses Yundt'),(33,'Dorian Schaden Jr.'),(34,'Alice Robel'),(35,'Celia Wisoky'),(36,'Edward Okuneva Jr.');
+INSERT INTO `authors` VALUES (31,'Sherlene Pfannerstill'),(32,'Ulysses Yundt'),(33,'Dorian Schaden Jr.'),(34,'Alice Robel'),(35,'Celia Wisoky'),(36,'Edward Okuneva Jr.'),(37,'Earle Erdman II'),(38,'Angelia Bednar'),(39,'Elmo Hammes'),(40,'Jean Glover'),(41,'Cliff Mills'),(42,'Guadalupe Hudson'),(43,'Alton Ziemann'),(44,'Javier Turner');
 /*!40000 ALTER TABLE `authors` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,7 +56,7 @@ CREATE TABLE `book_items` (
   UNIQUE KEY `unique_index` (`barcode`),
   KEY `book_id` (`book_id`),
   CONSTRAINT `book_items_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=67 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +65,7 @@ CREATE TABLE `book_items` (
 
 LOCK TABLES `book_items` WRITE;
 /*!40000 ALTER TABLE `book_items` DISABLE KEYS */;
-INSERT INTO `book_items` VALUES (45,41,'3921873765254'),(46,41,'4526657915314'),(47,41,'4526657915312'),(48,42,'5823574841917'),(49,42,'3374245303095'),(50,42,'2824961200136'),(56,44,'3648904798509'),(57,44,'9424374995471'),(59,45,'6074317430557'),(60,45,'3589416450168'),(62,46,'4140003214465'),(64,47,'1310065523659'),(65,47,'5495121750876');
+INSERT INTO `book_items` VALUES (45,41,'3921873765254'),(46,41,'4526657915314'),(47,41,'4526657915312'),(48,42,'5823574841917'),(49,42,'3374245303095'),(50,42,'2824961200136'),(56,44,'3648904798509'),(57,44,'9424374995471'),(59,45,'6074317430557'),(60,45,'3589416450168'),(62,46,'4140003214465'),(64,47,'1310065523659'),(65,47,'5495121750876'),(67,48,'9686475880203'),(69,49,'3406067904805'),(70,49,'7307174079669'),(72,50,'1276470583391'),(73,50,'9293387629334'),(75,51,'6018310168116'),(76,51,'5145096137164'),(78,52,'0507868611909'),(79,52,'7407287873465'),(81,53,'6000057373564'),(82,53,'2928260235070'),(84,54,'2099050206874'),(85,54,'2806690685408'),(87,55,'4334568969094'),(88,55,'4398554261722');
 /*!40000 ALTER TABLE `book_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -94,7 +94,7 @@ CREATE TABLE `books` (
   CONSTRAINT `books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`),
   CONSTRAINT `books_ibfk_2` FOREIGN KEY (`rack_id`) REFERENCES `racks` (`id`),
   CONSTRAINT `books_ibfk_3` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +103,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (41,'In Death Ground',31,6,1924,5,'9791008345880','Martinus Nijhoff Publishers',1,977),(42,'Mr Standfast',32,29,1992,2,'9781960442352','Blackstaff Press',1,618),(43,'Mr Standfast',32,29,1992,2,'9781960442352','Blackstaff Press',1,618),(44,'A Glass of Blessings',33,14,1983,3,'9780871242495','Imperial War Museum',1,399),(45,'Tiger! Tiger!',34,14,1910,5,'9781936889655','Paulist Press',1,114),(46,'This Side of Paradise',35,38,2016,5,'9781074990336','Manning Publications',1,465),(47,'For a Breath I Tarry',36,27,1912,2,'9781703569803','Vintage Books',1,609);
+INSERT INTO `books` VALUES (41,'In Death Ground',31,6,1924,5,'9791008345880','Martinus Nijhoff Publishers',1,977),(42,'Mr Standfast',32,29,1992,2,'9781960442352','Blackstaff Press',1,618),(43,'Mr Standfast',32,29,1992,2,'9781960442352','Blackstaff Press',1,618),(44,'A Glass of Blessings',33,14,1983,3,'9780871242495','Imperial War Museum',1,399),(45,'Tiger! Tiger!',34,14,1910,5,'9781936889655','Paulist Press',1,114),(46,'This Side of Paradise',35,38,2016,5,'9781074990336','Manning Publications',1,465),(47,'For a Breath I Tarry',36,27,1912,2,'9781703569803','Vintage Books',1,609),(48,'A Darkling Plain',37,6,1918,7,'9791855647717','Black Library',1,14),(49,'Waiting for the Barbarians',38,19,1964,6,'9791766710074','Darakwon Press',1,276),(50,'In a Glass Darkly',39,19,1991,3,'9780032623521','Atlantic Books',1,413),(51,'Terrible Swift Sword',40,31,2010,5,'9791512126036','Atlas Press',1,344),(52,'Paths of Glory',41,7,2009,2,'9791720488759','Simon & Schuster',1,585),(53,'The Painted Veil',42,18,1915,4,'9781096665311','Marshall Cavendish',1,859),(54,'The Millstone',43,41,1923,6,'9781839500688','No Starch Press',1,1),(55,'Those Barren Leaves, Thrones, Dominations',44,23,1907,6,'9791878863682','Bella Books',1,701);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,7 +126,7 @@ CREATE TABLE `borrows` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `borrows_ibfk_1` FOREIGN KEY (`book_item_id`) REFERENCES `book_items` (`id`),
   CONSTRAINT `borrows_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -135,7 +135,7 @@ CREATE TABLE `borrows` (
 
 LOCK TABLES `borrows` WRITE;
 /*!40000 ALTER TABLE `borrows` DISABLE KEYS */;
-INSERT INTO `borrows` VALUES (8,62,8,'2021-08-08',NULL);
+INSERT INTO `borrows` VALUES (8,62,8,'2021-07-08',NULL),(9,64,9,'2021-08-19',NULL),(10,60,9,'2021-08-19',NULL),(11,57,9,'2021-08-19','2021-08-20'),(12,49,9,'2021-08-19',NULL);
 /*!40000 ALTER TABLE `borrows` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -187,7 +187,7 @@ CREATE TABLE `membership_cards` (
   UNIQUE KEY `user_id` (`user_id`) USING BTREE,
   UNIQUE KEY `barcode` (`barcode`) USING BTREE,
   CONSTRAINT `membership_cards_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -196,7 +196,7 @@ CREATE TABLE `membership_cards` (
 
 LOCK TABLES `membership_cards` WRITE;
 /*!40000 ALTER TABLE `membership_cards` DISABLE KEYS */;
-INSERT INTO `membership_cards` VALUES (16,'7501488386714','2021-08-08','2022-02-04',8);
+INSERT INTO `membership_cards` VALUES (16,'7501488386714','2021-08-08','2022-02-04',8),(17,'6093625825947','2021-08-19','2022-02-15',9);
 /*!40000 ALTER TABLE `membership_cards` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -235,19 +235,19 @@ DROP TABLE IF EXISTS `notifications`;
 CREATE TABLE `notifications` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint unsigned NOT NULL,
-  `book_id` bigint unsigned DEFAULT NULL,
+  `book_item_id` bigint unsigned DEFAULT NULL,
   `content` varchar(255) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
   `notification_type_id` bigint unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `user_id` (`user_id`),
-  KEY `book_id` (`book_id`),
+  KEY `book_id` (`book_item_id`),
   KEY `notification_type_id` (`notification_type_id`),
   CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `book_items` (`id`),
+  CONSTRAINT `notifications_ibfk_2` FOREIGN KEY (`book_item_id`) REFERENCES `book_items` (`id`),
   CONSTRAINT `notifications_ibfk_3` FOREIGN KEY (`notification_type_id`) REFERENCES `notification_types` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -256,6 +256,7 @@ CREATE TABLE `notifications` (
 
 LOCK TABLES `notifications` WRITE;
 /*!40000 ALTER TABLE `notifications` DISABLE KEYS */;
+INSERT INTO `notifications` VALUES (4,8,62,'The book you reserved, This Side of Paradise, is now available!','2021-08-18',1);
 /*!40000 ALTER TABLE `notifications` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -297,7 +298,9 @@ CREATE TABLE `reservations` (
   `user_id` bigint unsigned DEFAULT NULL,
   `reservation_date` date DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
+  UNIQUE KEY `id` (`id`),
+  KEY `book_item_id` (`book_item_id`),
+  CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`book_item_id`) REFERENCES `book_items` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -378,7 +381,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   KEY `role_id` (`role_id`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -387,7 +390,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','admin',1,'Ayas Nasih',1),(8,'user','user',4,'Ahmed Mohamed',1);
+INSERT INTO `users` VALUES (1,'admin','admin',1,'Ayas Nasih',1),(8,'user','user',4,'Ahmed Mohamed',1),(9,'junit','junit',3,'JUnit',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -404,4 +407,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-08-08 18:44:26
+-- Dump completed on 2021-08-21  6:23:52
